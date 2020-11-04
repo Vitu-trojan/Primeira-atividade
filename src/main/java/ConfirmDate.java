ddbf6faffc67a7e75de2175e20ea2472ea49bf5a
Victor Dantas <victor.dant17@gmail.com>
2020-11-03 22:43:23 -0300

    primeiro commit

---
+++ src/main/java/ConfirmDate.java
@@ -0,0 +1,24 @@
+import sun.util.calendar.CalendarSystem;
+import sun.util.resources.cldr.CalendarData;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+public class ConfirmDate {
+    public static void main(String[]args){
+        System.out.println("---Confirmação de idade---");
+        Calendar c = Calendar.getInstance();
+        int now = c.get(Calendar.YEAR);
+        Scanner data = new Scanner(System.in);
+        System.out.println("Digite o ano do seu nascimento:");
+
+        int year = data.nextInt();
+        int ynow = now - year;
+        if (ynow<18){
+            System.out.println("Sua idade é de " + ynow + " ano(s). Você é menor de idade.");
+        }
+        else {
+            System.out.println("Sua idade é de " + ynow + "ano(s). Você é maior de idade.");
+        }
+    }
+}
